8220bf7bcafa5fbdadd92699c89dd5aabd2e6098
syaeful16 <syaefulanas121@gmail.com>
2024-10-08 11:33:24 +0700

    add ID

--- models/user.go
+++ models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	Username string `gorm:"type:varchar(255); not null: unique" json:"username"`
-	Email    string `gorm:"type:varchar(255); not null: unique" json:"email"`
-	Password string `gorm:"type:varchar(255); not null" json:"password"`
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	Username string `gorm:"type:varchar(255); not null" json:"username" validate:"required"`
+	Email    string `gorm:"type:varchar(255); not null; unique" json:"email" validate:"required,email"`
+	Password string `gorm:"type:varchar(255); not null" json:"password" validate:"required,min=8"`
 	ModelDefault
 }
