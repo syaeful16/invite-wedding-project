0e304aca6d0f0e39491e8bcf602a5245ae226bbc
syaeful16 <syaefulanas121@gmail.com>
2024-10-11 15:22:14 +0700

    add function forgot password (send email & reset)

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -2,6 +2,7 @@ package authcontroller
 
 import (
 	"encoding/json"
+	"fmt"
 	"invite-wed/configs"
 	"invite-wed/helpers"
 	"invite-wed/models"
@@ -129,5 +130,120 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	})
 
 	response := map[string]string{"message": "Successful Logout"}
+	helpers.JsonResponse(w, http.StatusOK, response)
+}
+
+func ForgotPasswordEmail(w http.ResponseWriter, r *http.Request) {
+	var userInput struct {
+		Email string `json:"email" validate:"required,email"`
+	}
+
+	decode := json.NewDecoder(r.Body)
+	if err := decode.Decode(&userInput); err != nil {
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	if validate := helpers.Validation(userInput); validate != nil {
+		response := map[string]interface{}{"errors": validate}
+		helpers.JsonResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	var userExist models.User
+	if err := models.DB.Where("email = ?", userInput.Email).First(&userExist).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			response := map[string]string{"message": "Email tidak terdaftar"}
+			helpers.JsonResponse(w, http.StatusNotFound, response)
+			return
+		}
+
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	// Send email
+	fmt.Print(userExist.Email)
+	if err := helpers.SendGoMail(userExist.Email, "token"); err != nil {
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	randToken, _ := helpers.GenerateRandomToken()
+
+	if err := models.DB.Create(&models.UserToken{Email: userExist.Email, Token: randToken}).Error; err != nil {
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	response := map[string]string{"message": "Token berhasil dikirim ke email"}
+	helpers.JsonResponse(w, http.StatusOK, response)
+}
+
+func ForgotPasswordReset(w http.ResponseWriter, r *http.Request) {
+	var userInput struct {
+		Passsword       string `json:"password" validate:"required,min=8"`
+		ConfirmPassword string `json:"confirm_password" validate:"required,min=8"`
+		Token           string `json:"token" validate:"required"`
+	}
+
+	decode := json.NewDecoder(r.Body)
+	if err := decode.Decode(&userInput); err != nil {
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	if validate := helpers.Validation(userInput); validate != nil {
+		response := map[string]interface{}{"errors": validate}
+		helpers.JsonResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	if userInput.Passsword != userInput.ConfirmPassword {
+		response := map[string]string{"message": "Password tidak sama"}
+		helpers.JsonResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	var userTokenExist models.UserToken
+	if err := models.DB.Where("token = ?", userInput.Token).First(&userTokenExist).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			response := map[string]string{"message": "Token tidak valid"}
+			helpers.JsonResponse(w, http.StatusNotFound, response)
+			return
+		}
+
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	if time.Since(userTokenExist.CreatedAt) > 10*time.Minute {
+		if err := models.DB.Where("token = ?", userTokenExist.Token).Delete(&models.UserToken{}).Error; err != nil {
+			response := map[string]string{"message": err.Error()}
+			helpers.JsonResponse(w, http.StatusInternalServerError, response)
+			return
+		}
+
+		response := map[string]string{"message": "Token sudah kadaluarsa"}
+		helpers.JsonResponse(w, http.StatusNotFound, response)
+		return
+	}
+
+	// Hash new password
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Passsword), bcrypt.DefaultCost)
+	// Update password
+	if err := models.DB.Model(&models.User{}).Where("email = ?", userTokenExist.Email).Update("password", string(hashedPassword)).Error; err != nil {
+		response := map[string]string{"message": err.Error()}
+		helpers.JsonResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	response := map[string]string{"message": "Berhasil reset password"}
 	helpers.JsonResponse(w, http.StatusOK, response)
 }
