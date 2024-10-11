dd7a19e58853e4db8d97a9d798091a8a8d3324c9
syaeful16 <syaefulanas121@gmail.com>
2024-10-11 15:22:45 +0700

    add generate token for reset password

---
+++ helpers/generateRandomToken.go
@@ -0,0 +1,16 @@
+package helpers
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func GenerateRandomToken() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(b), nil
+}
