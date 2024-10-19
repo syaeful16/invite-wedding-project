89190296d77b8c9c845e8c1d071b1d8da2aa4b14
syaeful16 <syaefulanas121@gmail.com>
2024-10-19 19:38:03 +0700

    add route invitation

--- models/setup.go
+++ models/setup.go
@@ -1,10 +1,13 @@
 package models
 
 import (
+	"log"
+	"os"
 	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type ModelDefault struct {
@@ -18,12 +21,22 @@ var DB *gorm.DB
 func ConnectDB() {
 	mysqlConf := "root:@tcp(localhost:3306)/invite_wedd?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(mysqlConf), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // Log to stdout
+		logger.Config{
+			SlowThreshold:             time.Second, // Log slow SQL queries (default: 200ms)
+			LogLevel:                  logger.Info, // Log level (e.g., Info, Warn, Error)
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Enable colorful output
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(mysqlConf), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		panic("Error connecting to database")
 	}
 
-	if err := db.AutoMigrate(&User{}, &UserToken{}); err != nil {
+	if err := db.AutoMigrate(&User{}, &UserToken{}, &Invitation{}, &Bride{}); err != nil {
 		panic("Error migrating database")
 	}
 
