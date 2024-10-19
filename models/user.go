1a271bd7b694527404f7fe5a1b79fd8d8d915992
syaeful16 <syaefulanas121@gmail.com>
2024-10-19 19:38:27 +0700

    add relation invitation im user

--- models/user.go
+++ models/user.go
@@ -1,10 +1,11 @@
 package models
 
 type User struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Username  string    `gorm:"type:varchar(255); not null" json:"username" validate:"required"`
-	Email     string    `gorm:"type:varchar(255); not null; unique" json:"email" validate:"required,email"`
-	Password  string    `gorm:"type:varchar(255); not null" json:"password" validate:"required,min=8"`
-	UserToken UserToken `gorm:"foreignKey:Email; references:Email" json:"-" validate:"-"`
+	ID         uint         `gorm:"primaryKey" json:"id"`
+	Username   string       `gorm:"type:varchar(255); not null" json:"username" validate:"required"`
+	Email      string       `gorm:"type:varchar(255); not null; unique" json:"email" validate:"required,email"`
+	Password   string       `gorm:"type:varchar(255); not null" json:"password" validate:"required,min=8"`
+	UserToken  UserToken    `gorm:"foreignKey:Email; references:Email" json:"-" validate:"-"`
+	Invitation []Invitation `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	ModelDefault
 }
